17fd9ea72c31f95ce90d9b42a257be593d87ab96
SumayyaVN9 <suminoushad1012gmail.com>
2025-09-21 18:13:13 +0530

    Updated App.css and added About.js

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+export const About = () => {
+  return (
+    <div className="about-container">
+      <h3>Hi Everyone,</h3>
+      <p>
+        I am currently pursuing a vocational degree in Software Application Development at CUSAT, 
+        with an interest in full-stack development using the MERN stack. 
+        I recently completed my Bachelor of Computer Applications (BCA) from 
+         MES College, Marampally. My goal is to secure a respected position 
+        in a reputed company and continue growing professionally in the field of 
+         software development.
+      </p>
+    </div>
+  );
+};
