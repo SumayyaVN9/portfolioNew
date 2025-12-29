63e7c5cf35a938c1c94335107b6ea1fbe0de5c8e
SumayyaVN9 <suminoushad101@gmail.com>
2025-12-29 15:22:16 +0530

    Update skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,31 +1,41 @@
 import { Col, Container, Row } from "react-bootstrap";
 
 export const Skills = () => {
-  const skillsList = [
-    "MongoDB",
-    "Figma",
-    "CSS",
-    "MERN Stack",
-    "javascript",
-    "XML",
-    "HTML",
-    "Python"
-  ];
+  const skills = {
+    Databases: ["MongoDB", "PostgreSQL"],
+
+    "Programming Languages": ["Java", "JavaScript"],
+
+    "Frontend Technologies": ["HTML", "CSS", "React.js", "Next.js"],
+
+    "Backend Technologies": ["Node.js", "Express.js"],
+
+    Tools: ["VS Code", "Git", "Figma"]
+  };
 
   return (
     <section className="skill" id="skills">
       <Container>
         <Row className="justify-content-center">
           <Col xs={12} md={10}>
-            <div className="skill-bx text-center">
+            <div className="skill-bx ">
               <h2>Skills</h2>
               <p>
-               I have worked with these skills in my projects and am eager to grow further.   </p>
-              <div className="skills-row">
-                {skillsList.map((skill, index) => (
-                  <div className="skill-item" key={index}>{skill}</div>
-                ))}
-              </div>
+                I have worked with these skills in my projects and am eager to grow further. 
+              </p>
+
+              {Object.entries(skills).map(([category, items], index) => (
+                <div className="skill-category" key={index}>
+                  <h4>{category}</h4>
+                  <div className="skills-row"> 
+                    <h5>: </h5>{items.map((skill, i) => (
+                      <div className="skill-item" key={i}>
+                        {skill}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
             </div>
           </Col>
         </Row>
